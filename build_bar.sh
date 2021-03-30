3d6aa06a44f8726ca45e18cb9b27668730b59249
thomaskrenn <thomas.krenn@kabsi.at>
2021-03-30 13:26:32 -0400

    Linux support

---
+++ build_bar.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# BAR configure, build and install commands
+# configure, build install project 'BAR'
+
+if [ ! -d bar ]
+then 
+  echo "'bar' doesn't exists "
+  exit 1
+fi
+
+if [ ! -d bar_build ]
+then 
+ mkdir bar_build
+fi
+
+if [ ! -d bar_install ]
+then 
+ mkdir bar_install
+fi
+
+install_path="/home/tom/github.com/cmake_pkg/bar_install"
+build_type=Release
+
+echo
+echo --------- cmake configure BAR -----------
+cmake -DCMAKE_INSTALL_PREFIX=${install_path} -DCMAKE_BUILD_TYPE=${build_type} -G  "Unix Makefiles" -B bar_build -S bar
+
+echo
+echo --------- cmake build BAR ----------
+cmake --build bar_build --config Release
+
+echo
+echo ---------- cmake install BAR ----------
+cmake --install bar_build --config Release
+
---
+++ build_usepkg.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# configure, build install project 'usepkg'
+
+if [ ! -d usepkg ]
+then 
+  echo "'usepkg' doesn't exists "
+  exit 1
+fi
+
+if [ ! -d usepkg_build ]
+then 
+ mkdir usepkg_build
+fi
+
+if [ ! -d usepkg_install ]
+then 
+ mkdir usepkg_install
+fi
+
+prefix_path=/home/tom/github.com/cmake_pkg/bar_install/cmake
+install_path=/home/tom/github.com/cmake_pkg/usepkg_install
+echo
+echo --------- cmake configure usepkg -----------
+cmake -DCMAKE_INSTALL_PREFIX=${install_path} -DCMAKE_PREFIX_PATH=${prefix_path} -G "Unix Makefiles" -B usepkg_build -S usepkg
+
+echo
+echo --------- cmake build usepkg ----------
+cmake --build usepkg_build --config Release
+
+echo
+echo ---------- cmake install usepkg ----------
+cmake --install usepkg_build --config Release
+
+#
+# List elf header of binary
+#
+readelf -d usepkg_install/bin/usepkg | head -20
+
